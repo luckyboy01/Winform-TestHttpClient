1d5e529eeec3721dfc88e1fcd6c56d2a8a5b2800
崔运发 <cuiyunfa@acctrue.com>
2025-07-31 17:12:55 +0800

    初始化

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Winform_TestHttpClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBox = new System.Windows.Forms.ListBox();
+            this.txtPostData = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(129, 271);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(100, 21);
+            this.txtTime.TabIndex = 0;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(129, 37);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(612, 21);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "开始测试";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "请求地址：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "请求次数：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 301);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "返回结果：";
+            // 
+            // txtBox
+            // 
+            this.txtBox.FormattingEnabled = true;
+            this.txtBox.ItemHeight = 12;
+            this.txtBox.Location = new System.Drawing.Point(129, 298);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(612, 148);
+            this.txtBox.TabIndex = 7;
+            // 
+            // txtPostData
+            // 
+            this.txtPostData.Location = new System.Drawing.Point(129, 64);
+            this.txtPostData.MaxLength = 999999999;
+            this.txtPostData.Multiline = true;
+            this.txtPostData.Name = "txtPostData";
+            this.txtPostData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtPostData.Size = new System.Drawing.Size(612, 201);
+            this.txtPostData.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "请求报文：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPostData);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.txtTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox txtBox;
+        private System.Windows.Forms.TextBox txtPostData;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Winform_TestHttpClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtUrl.Text = "https://ditest.oklinklink.com:8150/taskScheduler/asyncRun/relationupload";
+            txtUrl.Text = "http://10.10.100.21:8150/taskScheduler/asyncRun/productiondata";
+            //txtPostData.Text = "{\"orderCode\":\"T240123\",\"productLineCode\":\"trayLine\",\"workShop\":\"admin\",\"matCode\":\"123\",\"packageRate\":\"1:6\",\"batchNo\":\"240123\",\"subBatchNo\":\"\",\"productDate\":\"2024-01-01\",\"invalidDate\":\"2026-01-01\",\"pinBatchNo\":null,\"pinProductDate\":null,\"pinInvalidDate\":null,\"productCode\":\"1914701\",\"productName\":\"碘海醇注射液\",\"subNo\":\"1914701005\",\"linkProductCode\":\"1914701005\",\"assCorpCode\":\"\",\"qty\":2,\"palletQty\":1,\"cartonQty\":2,\"bundleQty\":0,\"unitQty\":12,\"isLast\":0,\"remark\":null,\"remark1\":null,\"remark2\":null,\"data\":[{\"parentPackCode\":null,\"packCode\":\"9147010001\",\"childQty\":2,\"pinChildQty\":0,\"packFlag\":0,\"levelNo\":9,\"data\":[{\"parentPackCode\":\"9147010001\",\"packCode\":\"88632390000000194105\",\"childQty\":6,\"pinChildQty\":0,\"packFlag\":0,\"levelNo\":2,\"data\":[{\"parentPackCode\":\"88632390000000194105\",\"packCode\":\"81578820000000556674\",\"childQty\":0,\"pinChildQty\":0,\"packFlag\":0,\"levelNo\":1,\"data\":null},{\"parentPackCode\":\"88632390000000194105\",\"packCode\":\"81578820000000545136\",\"childQty\":0,\"pinChildQty\":0,\"packFlag\":0,\"levelNo\":1,\"data\":null},{\"parentPackCode\":\"88632390000000194105\",\"packCode\":\"81578820000000523731\",\"childQty\":0,\"pinChildQty\":0,\"packFlag\":0,\"levelNo\":1,\"data\":null},{\"parentPackCode\":\"88632390000000194105\",\"packCode\":\"81578820000000539003\",\"childQty\":0,\"pinChildQty\":0,\"packFlag\":0,\"levelNo\":1,\"data\":null},{\"parentPackCode\":\"88632390000000194105\",\"packCode\":\"81578820000000502670\",\"childQty\":0,\"pinChildQty\":0,\"packFlag\":0,\"levelNo\":1,\"data\":null},{\"parentPackCode\":\"88632390000000194105\",\"packCode\":\"81578820000000511542\",\"childQty\":0,\"pinChildQty\":0,\"packFlag\":0,\"levelNo\":1,\"data\":null}]},{\"parentPackCode\":\"9147010001\",\"packCode\":\"88632390000000208421\",\"childQty\":6,\"pinChildQty\":0,\"packFlag\":0,\"levelNo\":2,\"data\":[{\"parentPackCode\":\"88632390000000208421\",\"packCode\":\"81578820000000619215\",\"childQty\":0,\"pinChildQty\":0,\"packFlag\":0,\"levelNo\":1,\"data\":null},{\"parentPackCode\":\"88632390000000208421\",\"packCode\":\"81578820000000560206\",\"childQty\":0,\"pinChildQty\":0,\"packFlag\":0,\"levelNo\":1,\"data\":null},{\"parentPackCode\":\"88632390000000208421\",\"packCode\":\"81578820000000578764\",\"childQty\":0,\"pinChildQty\":0,\"packFlag\":0,\"levelNo\":1,\"data\":null},{\"parentPackCode\":\"88632390000000208421\",\"packCode\":\"81578820000000582375\",\"childQty\":0,\"pinChildQty\":0,\"packFlag\":0,\"levelNo\":1,\"data\":null},{\"parentPackCode\":\"88632390000000208421\",\"packCode\":\"81578820000000599670\",\"childQty\":0,\"pinChildQty\":0,\"packFlag\":0,\"levelNo\":1,\"data\":null},{\"parentPackCode\":\"88632390000000208421\",\"packCode\":\"81578820000000602066\",\"childQty\":0,\"pinChildQty\":0,\"packFlag\":0,\"levelNo\":1,\"data\":null}]}]}]}";
+            txtTime.Text = "1";
+            txtUrl.Text = "https://ecd-d.bgnesz.com:8150/taskScheduler/asyncRun/rebackdata";
+            //txtUrl.Text = "https://ecd-d.bgnesz.com:8150/taskScheduler/asyncRun/rebackdata";
+            txtPostData.Text = "{\"orderCode\":\"T240123\",\"productLineCode\":\"T240123\",\"matCode\":null,\"batchNo\":\"240123\",\"subBatchNo\":\"\",\"productCode\":\"1914701\",\"productName\":\"碘海醇注射液\",\"subNo\":\"1914701003\",\"operateType\":3,\"packCode\":\"88000130000000005615\",\"levelNo\":2,\"secPackCode\":null,\"remark\":null,\"remark1\":null,\"remark2\":null}";
+            //var filePath = Environment.CurrentDirectory + "\\" + DateTime.Now.ToString("yyyy-MM-dd") + ".txt";
+            //if (!File.Exists(filePath))
+            //{
+            //    File.Create(filePath);
+            //}
+        }
+
+        /// <summary>
+        /// http请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="header"></param>
+        /// <param name="postData"></param>
+        /// <returns></returns>
+        public static string GetHttpWebResponse(string url, string header, object postData)
+        {
+            HttpWebRequest request = null;
+            if (url.StartsWith("https", StringComparison.OrdinalIgnoreCase))
+            {
+                // 这里设置了协议类型。
+                ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;// SecurityProtocolType.Tls1.2; 
+                request = WebRequest.Create(url) as HttpWebRequest;
+                ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+                request.ProtocolVersion = HttpVersion.Version11;
+                request.KeepAlive = false;
+                request.AllowAutoRedirect = true;
+                ServicePointManager.CheckCertificateRevocationList = false;
+                ServicePointManager.DefaultConnectionLimit = 100;
+                ServicePointManager.Expect100Continue = false;
+
+            }
+            else
+            {
+                request = (HttpWebRequest)WebRequest.Create(url);
+            }
+
+            if (!string.IsNullOrEmpty(header))
+            {
+                request.Headers.Add("Authorization", header);
+            }
+
+            request.Method = "POST";
+            request.ContentType = "application/json;charset=UTF-8";
+            if (url.Contains("UploadCollectionRates") || url.Contains("Services/ScanCodeService.asmx"))
+            {
+                request.ContentType = "text/xml;charset=utf-8";
+            }
+            //request.ContentLength = postData.Length;
+            request.Timeout = 5000;
+
+            HttpWebResponse response = null;
+
+            try
+            {
+                StreamWriter swRequestWriter = new StreamWriter(request.GetRequestStream());
+                swRequestWriter.Write(postData);
+
+                if (swRequestWriter != null)
+                    swRequestWriter.Close();
+
+                response = (HttpWebResponse)request.GetResponse();
+                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                if (ex.Response == null)
+                    throw ex;
+
+                HttpWebResponse exResponse = (HttpWebResponse)ex.Response;
+                //500错误
+                if (exResponse.StatusCode == HttpStatusCode.InternalServerError)
+                {
+                    using (var stream = exResponse.GetResponseStream())
+                    using (var reader = new StreamReader(stream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+                if (exResponse.StatusCode == HttpStatusCode.BadRequest)
+                {
+                    using (var stream = exResponse.GetResponseStream())
+                    using (var reader = new StreamReader(stream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+                else
+                {
+                    throw ex;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (response != null)
+                    response.Close();
+                if (request != null)
+                    request.Abort();
+            }
+        }
+
+        private static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+        {
+            return true; //总是接受  
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int times = 0;
+            if(string.IsNullOrWhiteSpace(txtUrl.Text))
+            {
+                MessageBox.Show("请输入请求地址","警告", MessageBoxButtons.OK);
+                return;
+            }
+            if(string.IsNullOrWhiteSpace(txtPostData.Text))
+            {
+                MessageBox.Show("请输入请求报文","警告", MessageBoxButtons.OK);
+                return;
+            }
+            int.TryParse(txtTime.Text, out times);
+            if(times <= 0)
+            {
+
+                MessageBox.Show("请求次数请输入正整数", "警告", MessageBoxButtons.OK);
+                return;
+            }
+            var res = string.Empty;
+            for (int i = 0; i < times; i++)
+            {
+                try
+                {
+                    res = GetHttpWebResponse(txtUrl.Text.Trim(), "", txtPostData.Text) + "\r\n";
+                }
+                catch (Exception ex)
+                {
+                    res = ex.Message + "\r\n";
+                }
+                Thread.Sleep(100);
+                AppendMsg(res);
+                //txtResult.Text = res;
+            }
+        }
+        void AppendMsg(string msg)
+        {
+            msg = string.Concat("[", DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss:fff"), "]:", msg);
+            txtBox.Items.Add(msg);
+            this.txtBox.SelectedIndex = this.txtBox.Items.Count - 1;
+            this.txtBox.SelectedIndex = -1;
+            var filePath = Environment.CurrentDirectory + "\\" + DateTime.Now.ToString("yyyy-MM-dd") + ".txt";
+            ////if (!File.Exists(filePath))
+            ////    File.Create(filePath);
+            ////File.WriteAllText(filePath, msg);
+            using (StreamWriter writer = new StreamWriter(filePath, true)) // true 代表追加模式
+            {
+                writer.WriteLine(msg);
+            }
+        }
+    }
+}
